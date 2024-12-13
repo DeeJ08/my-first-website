c51f3b465bfa965234e7fcf8495a704a38081fa1
Daisy Jimenez <jimenez.daisy9494@gmail.com>
2024-12-13 08:35:05 -0800

    added movement

--- script.js
+++ script.js
@@ -2,5 +2,19 @@ const h2 = document.createElement("h2");
 h2.textContent = "use arrow keys for movement";
 document.querySelector("body").appendChild(h2);
 
+const img = document.getElementById("cat");
+const changePoint = window.innerWidth - 90;
+const maxRight = window.innerWidth - img.width;
+const newImageSrc = "cat.png";
+let currentLeft = 30;
 
-
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowRight') {
+      if (currentLeft < maxRight) {
+        currentLeft += 10; 
+        if (currentLeft >= changePoint) {    
+        }
+        img.style.left = `${currentLeft}px`;
+      }  
+    }
+});
